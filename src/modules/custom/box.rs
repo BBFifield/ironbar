ec8ea4d4fc4d6f90b0d44080f1dc27905c5d3afe
Brandon <bb.fifield@gmail.com>
2025-05-18 01:11:07 -0230

    Create enum for halign options in box.rs

--- src/modules/custom/box.rs
+++ src/modules/custom/box.rs
@@ -7,6 +7,30 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone)]
 #[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
+pub enum ModuleAlignment {
+    /// Align widget to the start (left).
+    Start,
+    /// Align widget to the center.
+    Center,
+    /// Align widget to the end (right).
+    End,
+    /// Stretch widget to fill available space.
+    Fill,
+}
+
+impl From<ModuleAlignment> for gtk::Align {
+    fn from(align: ModuleAlignment) -> Self {
+        match align {
+            ModuleAlignment::Start => gtk::Align::Start,
+            ModuleAlignment::Center => gtk::Align::Center,
+            ModuleAlignment::End => gtk::Align::End,
+            ModuleAlignment::Fill => gtk::Align::Fill,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
 pub struct BoxWidget {
     /// Widget name.
     ///
@@ -21,9 +45,14 @@ pub struct BoxWidget {
     /// Whether child widgets should be horizontally or vertically added.
     ///
     /// **Valid options**: `horizontal`, `vertical`, `h`, `v`
-    /// <br />
     /// **Default**: `horizontal`
     orientation: Option<ModuleOrientation>,
+
+    /// Horizontal alignment for the box.
+    ///
+    /// **Valid options**: `start`, `center`, `end`, `fill`
+    /// **Default**: `fill`
+    halign: Option<ModuleAlignment>,
 
     /// Modules and widgets to add to this box.
     ///
@@ -36,21 +65,21 @@ impl CustomWidget for BoxWidget {
 
     fn into_widget(self, context: CustomWidgetContext) -> Self::Widget {
         let container = build!(self, Self::Widget);
-    
+
         if let Some(orientation) = self.orientation {
             container.set_orientation(orientation.into());
         }
-    
+
         if let Some(widgets) = self.widgets {
             for widget in widgets {
                 widget.widget.add_to(&container, &context, widget.common);
             }
         }
-    
-        // Set halign property to center
-        container.set_halign(gtk::Align::Center);
-    
+
+        // Use provided halign, defaulting to Center if not specified.
+        let alignment = self.halign.unwrap_or(ModuleAlignment::Fill);
+        container.set_halign(alignment.into());
+
         container
     }
-
 }
